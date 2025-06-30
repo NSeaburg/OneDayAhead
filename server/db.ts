c4cdcf15a95fcd9c32841737eae131ad1371a47e
NSeaburg <nate@onedayahead.com>
2025-06-30 04:34:27 +0000

    IncreaesDB Timeout

--- server/db.ts
+++ server/db.ts
@@ -18,7 +18,18 @@ export const pool = new Pool({
   },
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+  connectionTimeoutMillis: 10000, // Increased from 2000 to 10000ms
+  query_timeout: 10000, // Add query timeout
+  statement_timeout: 10000, // Add statement timeout
+});
+
+// Test connection on startup
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL database");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
 });
 
 // Initialize Drizzle with node-postgres adapter
